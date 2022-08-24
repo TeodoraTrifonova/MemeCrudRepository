0419f316c022ec8727e7fe8ed4e62a6a8783f9ce
TeodoraTrifonova <92271887+TeodoraTrifonova@users.noreply.github.com>
2022-08-24 12:56:14 +0300

    .

---
+++ MemeCrudProject/domain/src/test/java/tinqin/project/domain/DomainApplicationTests.java
@@ -0,0 +1,13 @@
+package tinqin.project.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
